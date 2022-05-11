a77d3bf10333d63db1de49ba28f54da0eba1ebf3
jahidhassan2424 <jahidhassan.programmer@gmail.com>
2022-05-11 06:30:24 +0600

    Banner and card working

--- src/components/Home/InfoCard.js
+++ src/components/Home/InfoCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const InfoCard = ({ img, title, bgClass }) => {
     return (
-        <div class={`card lg:card-side pl-1 lg:pl-12  bg-base-100 shadow-xl  ${bgClass}`} >
+        <div class={`card lg:card-side pl-1 lg:pl-12   shadow-xl  ${bgClass}`} >
             <figure><img src={img} alt="Album" /></figure>
             <div class="card-body text-white  ">
                 <h2 class="card-title">{title}</h2>
